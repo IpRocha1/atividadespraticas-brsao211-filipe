e26831e294982cbf7386ee74452968e9bc2a72c0
Filipe Ribeiro <liperrocha@gmail.com>
2026-01-08 20:07:50 -0300

    Atividade prática 01

---
+++ Atividade Prática 01/questao04.py
@@ -0,0 +1,10 @@
+nome_do_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_do_produto)
+print("Quantidade:", quantidade)
+print("Preço Unitário: R$ %.2f" % preco_unitario)
+print("Preço Total: R$ %.2f" % preco_total)
