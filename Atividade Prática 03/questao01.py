9b93e11b23ade14c7a2ed9e1ba0e2437f01ec49a
Filipe Ribeiro <liperrocha@gmail.com>
2026-01-12 20:49:06 -0300

    Ajuste na questão 01

--- Atividade Prática 03/questao01.py
+++ Atividade Prática 03/questao01.py
@@ -1,10 +1,10 @@
 idade = int(input("Digite sua idade: "))
 
-if idade >= 0 and idade <= 12:
+if idade <= 12:
     print("Criança (0-12 anos)")
-elif idade > 12 and idade <= 17:
+elif idade <= 17:
     print("Adolescente (13-17 anos)")
-elif idade > 17 and idade <= 59:
+elif idade <= 59:
     print("Adulto (18-59 anos)")
-elif idade >= 60:
+else:
     print("Idoso (60 anos ou mais)")
