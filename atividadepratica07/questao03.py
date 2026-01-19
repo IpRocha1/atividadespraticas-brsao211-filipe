86fd5022a5e4f5a5dba9e307fc4540126ebe7ac8
Filipe Ribeiro <liperrocha@gmail.com>
2026-01-19 20:47:30 -0300

    Ajuste nas questoes 03 e 04

--- atividadepratica07/questao03.py
+++ atividadepratica07/questao03.py
@@ -1,12 +1,15 @@
+import csv
 class LeituraArquivo:
     def __init__(self, arquivo):
         self.arquivo = f"atividadepratica07/{arquivo}"
 
     def exibir_conteudo(self):
         try:
-            with open(self.arquivo, 'r') as file:
-                for linha in file:
-                    print(linha.strip())
+            with open(self.arquivo, 'r', newline='', encoding ='utf-8') as file:
+                arquivo_csv = csv.reader(file)
+                print("Conteúdo do arquivo:")
+                for linha in arquivo_csv:
+                    print(', '.join(linha))                
         except FileNotFoundError:
             print(f"Erro: O arquivo '{self.arquivo}' não foi encontrado.")
         except Exception as e:
--- atividadepratica07/questao04.py
+++ atividadepratica07/questao04.py
@@ -1,4 +1,3 @@
-"Crie um programa que leia e escreva arquivos no formato , que salve em um dicionário com nome, idade e cidade em um arquivo JSON e depois leia o mesmo arquivo exibindo os dados, caso o arquivo não existir ou ocorrer erro ao salvar, mostre uma mensagem de falha."
 import json
 
 class Pessoa:
